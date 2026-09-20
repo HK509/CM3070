464ef0f319600da75c9521d93c7853bb7fb8f489
HK509 <hk282@student.london.ac.uk>
2026-09-20 17:51:22 +0100

    adding water pump - irrigation
    Adding water pump (irrigation) through low level trigger relay to mitigation node

--- Mitigation_Node.ino
+++ Mitigation_Node.ino
@@ -3,6 +3,9 @@
 #include <Servo.h>
 
 
+//declare water pump relay pin - irrigation system 
+const int WATER_PUMP_PIN = D7;
+
 //create a servo object - floodgate
 Servo floodgate;
 
@@ -10,6 +13,9 @@ Servo floodgate;
 //create and initialise a variable to store floodgate status, and its position
 bool floodgateActive = false;
 int floodgate_pos = 0;
+
+//create and initialise a variable to store irrigation system status
+bool irrigationActive = false;
 
 
 
@@ -19,8 +25,12 @@ void setup() {
   //begin serial monitor
   Serial.begin(9600);
 
-  //define servo pin - floodgate
+  //define servo pin - floodgate, and min pulse width (500 as 0°) and max pulse width (2500°)
   floodgate.attach(D2, 500, 2500);
+
+  //initialise water pump relay pin as output, and set it as off - HIGH because it is a low level trigger relay
+  pinMode(WATER_PUMP_PIN, OUTPUT);
+  digitalWrite(WATER_PUMP_PIN, HIGH);
 
 }
 
@@ -28,9 +38,11 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   activateFloodgate();
-  delay(500);
+  activateIrrigation();
+  delay(2000);
   deactivateFloodgate();
-  delay(500);
+  deactivateIrrigation();
+  delay(5000);
 
 }
 
@@ -65,4 +77,36 @@ void deactivateFloodgate(){
 
   Serial.print("Floodgate Active: ");
   Serial.println(floodgateActive);
+}
+
+
+
+//function that activates/ starts the irigation system - pump turns on
+void activateIrrigation(){
+  if(irrigationActive == false){
+    digitalWrite(WATER_PUMP_PIN, LOW);
+    //wait 2 seconds
+    delay(2000);
+  }
+
+  irrigationActive = true;
+
+  Serial.print("Irrigation Active: ");
+  Serial.println(irrigationActive);
+}
+
+
+
+//function that deactivates/ stops the irigation system - pump turns off
+void deactivateIrrigation(){
+  if(irrigationActive == true){
+    digitalWrite(WATER_PUMP_PIN, HIGH);
+    //wait 2 seconds
+    delay(2000);
+  }
+
+  irrigationActive = false;
+
+  Serial.print("Irrigation Active: ");
+  Serial.println(irrigationActive);
 }
